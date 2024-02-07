78685e81c3b52714b963e49feef480ff790e59f4
ICAS03 <111299269+ICAS03@users.noreply.github.com>
2024-02-07 10:23:31 +0800

    register screen

---
+++ lib/screens/onBoarding_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class onboardingScreen extends StatefulWidget {
+  const onboardingScreen({super.key});
+
+  @override
+  State<onboardingScreen> createState() => _onboardingState();
+}
+
+class _onboardingState extends State<onboardingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
